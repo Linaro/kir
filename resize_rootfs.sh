5afd720101f53b6dd548ede07d91e10187675a24
Anders Roxell <anders.roxell@linaro.org>
2019-10-23 16:24:52 +0200

    add a helper file
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- resize_rootfs.sh
+++ resize_rootfs.sh
@@ -12,47 +12,7 @@ OVERLAY_FILE=${2:-/lava-lxc/overlays/target/overlay.tar.gz}
 EXTRA_SIZE=${EXTRA_SIZE:-512000}
 SPARSE_NEEDED=${3:-no}
 
-find_extracted_size() {
-	local local_file=${1}
-	local local_file_type=${2}
-	local local_size=
-	if [[ ${local_file_type} = *"POSIX tar archive"* ]]; then
-		local_size=$(ls -l "${local_file}" | awk '{print $5}')
-		local_size=$(( "${local_size}" / 1024 ))
-	elif [[ ${local_file_type} = *"gzip compressed data"* ]]; then
-		local_size=$(gzip -l "${local_file}" | tail -1 | awk '{print $2}')
-		local_size=$(( "${local_size}" / 1024 ))
-	elif [[ ${local_file_type} = *"XZ compressed data"* ]]; then
-		local_size=$(xz -l "${local_file}" | tail -1 | awk '{print $5}'|sed 's/,//g' | awk -F'.' '{print $1}')
-		local_size=$(( "${local_size}"+1 ))
-		local_size=$(( "${local_size}" * 1024 ))
-	elif [[ ${local_file_type} = *"ext4 filesystem data"* ]]; then
-		local_size=$(ls -l "${local_file}" | awk '{print $5}')
-		local_size=$(( "${local_size}" / 1024 ))
-	else
-		echo "ABORT: Format not supported: ${local_size}"
-		exit 1
-	fi
-	echo ${local_size}
-}
-
-unpack_tar_file() {
-	local local_file=${1}
-	local local_mount_point=${2}
-	tar -xvf ${local_file} -C ${local_mount_point}
-}
-
-curl_me() {
-	local local_file=${1}
-	if [[ ! -f $(basename "${local_file}") ]]; then
-		curl -sSL -o "$(basename "${local_file}")" "${local_file}"
-		retcode=$?
-		if [[ ${retcode} -ne 0 ]]; then
-			exit ${retconde}
-		fi
-	fi
-	echo $(basename "${local_file}")
-}
+. $(dirname $0)/helper.sh
 
 if [[ -d /lava-lxc ]]; then
 	cd /lava-lxc
