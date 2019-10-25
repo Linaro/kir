067f45b60474f582d01c8befc9f0af46d7eab8e2
Anders Roxell <anders.roxell@linaro.org>
2019-10-25 16:13:48 +0200

    test_repack_boot: rework so it works
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- test_repack_boot.sh
+++ test_repack_boot.sh
@@ -8,43 +8,48 @@ get_artifact() {
 	echo ${artifact}
 }
 
-base=http://snapshots.linaro.org/openembedded/lkft/lkft
-oe_version=sumo
-machine=hikey
-linux=linux-next
-ROOTFS_BUILDNR=${ROOTFS_BUILDNR:-590}
-BUILDNR=${ROOTFS_BUILDNR}
+base="http://snapshots.linaro.org/openembedded/lkft/lkft"
+oe_version="sumo"
+linux="linux-mainline"
+ROOTFS_BUILDNR="2140"
 
-url="${base}"/"${oe_version}"/"${machine}"/lkft/"${linux}"/"${BUILDNR}"
 
 # Hikey test
-curl -s "${url}" -o index.txt
-rootfs=$(get_artifact "rpb-console-image-lkft-" ".ext4.gz")
-echo ${rootfs}
+machine=hikey
+BUILDNR=${ROOTFS_BUILDNR}
+url="${base}"/"${oe_version}"/"${machine}"/lkft/"${linux}"/"${BUILDNR}"
+curl -sL "${url}" -o index.txt
+rootfs="${url}/$(get_artifact "rpb-console-image-lkft-" ".ext4.gz")"
 mv index.txt hikey-rootfs.index.txt
 BUILDNR=latest
-curl -s "${url}" -qo index.txt
-modules=$(get_artifact "modules-" ".tgz")
-dtb=$(get_artifact "Image-" ".dtb")
-kernel=$(get_artifact "Image-" ".bin")
+url="${base}"/"${oe_version}"/"${machine}"/lkft/"${linux}"/"${BUILDNR}"
+curl -sL "${url}" -o index.txt
+modules="${url}/$(get_artifact "modules-" ".tgz")"
+dtb="${url}/$(get_artifact "Image-" ".dtb")"
+kernel="${url}/$(get_artifact "Image-" ".bin")"
 mv index.txt hikey-artifacts.index.txt
 
-echo ./repack_boot.sh -t ${machine} -f "${url}"/"${rootfs}" -d "${url}"/"${dtb}" -k "${url}"/"${kernel}" -m "${url}"/"${modules}"
+echo
+echo TARGET: ${machine}
+echo ./repack_boot.sh -t ${machine} -f "${rootfs}" -d "${dtb}" -k "${kernel}" -m "${modules}"
 
 # am57xx-evm test
 machine=am57xx-evm
-BUILDNR=${ROOTFS_BUILDNR}
+BUILDNR="${ROOTFS_BUILDNR}"
+url="${base}"/"${oe_version}"/"${machine}"/lkft/"${linux}"/"${BUILDNR}"
 curl -sL "${url}" -o index.txt
-rootfs=$(get_artifact "rpb-console-image-lkft-" ".ext4.gz")
+rootfs="${url}/$(get_artifact "rpb-console-image-lkft-" ".ext4.gz")"
 mv index.txt x15-rootfs.index.txt
-BUILDNR=latest
+BUILDNR="latest"
+url="${base}"/"${oe_version}"/"${machine}"/lkft/"${linux}"/"${BUILDNR}"
 curl -sL "${url}" -qo index.txt
-modules=$(get_artifact "modules-" ".tgz")
-dtb=$(get_artifact "zImage-" ".dtb")
-kernel=$(get_artifact "zImage-" ".bin")
-echo KERNEL: ${kernel}
+modules="${url}/$(get_artifact "modules-" ".tgz")"
+dtb="${url}/$(get_artifact "zImage-" ".dtb")"
+kernel="${url}/$(get_artifact "zImage-" ".bin")"
 mv index.txt x15-artifacts.index.txt
 
-echo ./repack_boot.sh -t ${machine} -f "${url}"/"${rootfs}" -d "${url}"/"${dtb}" -k "${url}"/"${kernel}" -m "${url}"/"${modules}"
+echo
+echo TARGET: ${machine}
+echo ./repack_boot.sh -t ${machine} -f "${rootfs}" -d "${dtb}" -k "${kernel}" -m "${modules}"
 
 rm *.index.txt
