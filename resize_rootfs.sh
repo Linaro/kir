3abbfad43450ac337bd192007076f93c28a6623c
Anders Roxell <anders.roxell@linaro.org>
2019-10-28 14:33:15 +0100

    resize_rootfs: introduce OPTARG
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- resize_rootfs.sh
+++ resize_rootfs.sh
@@ -2,10 +2,9 @@
 
 #set -xe
 
-LXC_ROOTFS_FILE=${1}
-OVERLAY_FILE=${2:-/lava-lxc/overlays/target/overlay.tar.gz}
 EXTRA_SIZE=${EXTRA_SIZE:-64000}
-SPARSE_NEEDED=${3:-no}
+sparse_needed=0
+clear_modules=0
 
 . $(dirname $0)/helper.sh
 
@@ -16,12 +15,47 @@ else
 	cd $(pwd)/lava-lxc
 fi
 
-OVERLAY_FILE=$(curl_me "${OVERLAY_FILE}")
-LXC_ROOTFS_FILE=$(curl_me "${LXC_ROOTFS_FILE}")
+usage() {
+	echo -e "$0's help text"
+	echo -e "   -c, cleanup pre-installed modules in /lib/modules/"
+	echo -e "      before we install the new one, default: 0"
+	echo -e "   -f ROOTFS_URL, specify a url to a rootfs, either a (ext4|tar).(gz|xz)."
+	echo -e "      Can be to a file on disk: file:///path/to/file.gz"
+	echo -e "   -o OVERLAY_URL, specify a url to a kernel module tgz file."
+	echo -e "      Can be to a file on disk: file:///path/to/file.gz"
+	echo -e "   -s SPARSE imge or not"
+	echo -e "   -t TARGET, add machine name"
+	echo -e "   -h, prints out this help"
+}
 
-overlay_file_type=$(file "${OVERLAY_FILE}")
+while getopts "cd:f:hm:o:s" arg; do
+	case $arg in
+	c)
+		clear_modules=1
+		;;
+	f)
+		LXC_ROOTFS_URL="$OPTARG"
+		;;
+	o)
+		LXC_OVERLAY_URL="$OPTARG"
+		;;
+	s)
+		sparse_needed=1
+		;;
+	h|*)
+		usage
+		exit 0
+		;;
+	esac
+done
+
+
+LXC_OVERLAY_FILE=$(curl_me "${LXC_OVERLAY_URL}")
+LXC_ROOTFS_FILE=$(curl_me "${LXC_ROOTFS_URL}")
+
+overlay_file_type=$(file "${LXC_OVERLAY_FILE}")
 rootfs_file_type=$(file "${LXC_ROOTFS_FILE}")
-overlay_size=$(find_extracted_size "${OVERLAY_FILE}" "${overlay_file_type}")
+overlay_size=$(find_extracted_size "${LXC_OVERLAY_FILE}" "${overlay_file_type}")
 rootfs_size=$(find_extracted_size "${LXC_ROOTFS_FILE}" "${rootfs_file_type}")
 
 mount_point_dir=$(get_mountpoint_dir)
@@ -42,7 +76,10 @@ if [[ "${LXC_ROOTFS_FILE}" =~ ^.*.tar* ]]; then
 	unpack_tar_file "${LXC_ROOTFS_FILE}" "${mount_point_dir}"
 fi
 
-unpack_tar_file "${OVERLAY_FILE}" "${mount_point_dir}"
+if [[ $clear_modules -eq 1 ]]; then
+	rm -rf "${mount_point_dir}"/lib/modules/*
+fi
+unpack_tar_file "${LXC_OVERLAY_FILE}" "${mount_point_dir}"
 
 if [[ "${LXC_ROOTFS_FILE}" =~ ^.*.tar* ]]; then
 	cd "${mount_point_dir}"
@@ -52,7 +89,7 @@ fi
 
 loopback_unmount "${mount_point_dir}"
 
-if [[ "${SPARSE_NEEDED}" = *"yes"* ]]; then
+if [[ ${sparse_needed} -eq 1 ]]; then
 	img_file="$(basename "${new_file_name}" .ext4).img"
 	create_a_sparse_xz_img "${img_file}" "${new_file_name}"
 else
