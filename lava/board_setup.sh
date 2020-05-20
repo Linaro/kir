21045a9993a5392c6a93d163ea0b4ff3d112b5b0
Anders Roxell <anders.roxell@linaro.org>
2020-05-20 15:10:30 +0200

    lava: board_setup: re-enable rootfs.img for db410c
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- lava/board_setup.sh
+++ lava/board_setup.sh
@@ -29,7 +29,15 @@ case ${DEVICE_TYPE} in
 		machine=am57xx-evm
 		${kir}/repack_boot.sh -t "${machine}" -f "${local_rootfs}" -d "${local_dtb}" -k "${local_kernel}" -m "${local_modules}"
 		;;
-	dragonboard-410c|dragonboard-845c)
+	dragonboard-410c)
+		local_dtb=$(find . -type f -name '*.dtb')
+		echo "PRINTOUT DTB: ${local_dtb}"
+		file ${local_dtb}
+		machine=${DEVICE_TYPE}
+		${kir}/repack_boot.sh -t "${machine}" -d "${local_dtb}" -k "${local_kernel}"
+		${kir}/resize_rootfs.sh -s -f "${local_rootfs_ext4}" -o "${local_modules}"
+		;;
+	dragonboard-845c)
 
 		local_dtb=$(find . -type f -name '*.dtb')
 		echo "PRINTOUT DTB: ${local_dtb}"
