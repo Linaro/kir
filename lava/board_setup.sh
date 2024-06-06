Linaro/kir

commit bd27d7a5bb42e89b0af63e657c927cc94a3c1ffa

lava: boot_setup: use MODULES_PATH variable
Default MODULES_PATH to '/'.

Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

lava/board_setup.sh
@@ -6,6 +6,7 @@ set -e
 
 DEVICE_TYPE=${1}
 ROOTFS_STRING=${2:-"image-"}
+MODULES_PATH=${3:-"/"}
 kir=$(dirname $0)/..
 
 echo "PRINTOUT"
@@ -45,7 +46,7 @@ case ${DEVICE_TYPE} in
 				${kir}/repack_boot.sh -t "${machine}" -d "${local_dtb}" -k "${local_kernel}"
 				;;
 		esac
-		${kir}/resize_rootfs.sh -s -f "${local_rootfs}" -o "${local_modules}"
+		${kir}/resize_rootfs.sh -s -f "${local_rootfs}" -o "${local_modules}" -p "${MODULES_PATH}"
 		;;
 	nfs-dragonboard-845c)
 

commit 1d3cf523206e783c3a394f6c1660eed9274b69dc

lava: boot_setup: x15: fix MODULES_PATH
Missed module_path for x15.

Fixes: bd27d7a5bb42 ("lava: boot_setup: use MODULES_PATH variable")
Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

lava/board_setup.sh
@@ -31,7 +31,7 @@ case ${DEVICE_TYPE} in
 		echo "PRINTOUT DTB: ${local_dtb}"
 		file ${local_dtb}
 		machine=am57xx-evm
-		${kir}/repack_boot.sh -t "${machine}" -f "${local_rootfs}" -d "${local_dtb}" -k "${local_kernel}" -m "${local_modules}"
+		${kir}/repack_boot.sh -t "${machine}" -f "${local_rootfs}" -d "${local_dtb}" -k "${local_kernel}" -m "${local_modules}" -p "${MODULES_PATH}"
 		;;
 	dragonboard-410c|dragonboard-845c|e850-96|qrb5165-rb5)
 		local_dtb=$(find . -type f -name '*.dtb')